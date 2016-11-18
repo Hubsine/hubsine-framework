527c8569556e8abaeb1c4a4aebecb8456b11b165
Hubsine <contact@hubsine.com>
2016-11-19 00:27:23 +0100

    - Create wp entity

---
+++ src/Entity/User.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Hubsine\Framework\Entity;
+
+/**
+ * User
+ * 
+ * @property string $nickname
+ * @property string $description
+ * @property string $user_description
+ * @property string $first_name
+ * @property string $user_firstname
+ * @property string $last_name
+ * @property string $user_lastname
+ * @property string $user_login
+ * @property string $user_pass
+ * @property string $user_nicename
+ * @property string $user_email
+ * @property string $user_url
+ * @property string $user_registered
+ * @property string $user_activation_key
+ * @property string $user_status
+ * @property string $display_name
+ * @property string $spam
+ * @property string $deleted
+ *
+ * @author Hubsine
+ */
+class User extends \WP_User{
+    
+    public function __construct() {
+        
+        parent::__construct();
+    }
+}
---
+++ src/Entity/UserMeta.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Hubsine\Framework\Entity;
+
+/**
+ * UserMeta
+ *
+ * @author Hubsine
+ */
+class UserMeta {
+    
+    /**
+     *
+     * @var int user_meta
+     */
+    public $user_id;
+    
+    /**
+     *
+     * @var string user_meta
+     */
+    public $meta_key;
+    
+    /**
+     *
+     * @var string user_meta
+     */
+    public $meta_value;
+}
